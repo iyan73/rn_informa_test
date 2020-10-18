3dac1d5f844aec066db37060b868b8ca43c10b5e
Just-Iyan <nurdiansyah@technosoft.com.sg>
2020-10-18 21:38:06 +0700

    refactor intro screen

--- screens/Intro.js
+++ screens/Intro.js
@@ -1,15 +1,19 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Text, TouchableOpacity } from 'react-native';
 import { Container, Content, Form, Item, Input, Label } from 'native-base';
 import { LinearGradient } from 'expo-linear-gradient';
 import AsyncStorage from '@react-native-community/async-storage';
+import { TextInput } from 'react-native-paper';
 
 const Intro = ({navigation}) => {
+
+    const [name, setName] = useState("")
 
     const simpanData = async () => {
         try {
             await AsyncStorage .setItem('INFORMA', "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFuZ3VsYXJAZ21haWwuY29tIiwidXNlcklkIjoiNGMyNzkyMDAtNDVkZS00ZTY5LTg5MTItMzAxMjY2N2JjMzlkIiwiaWF0IjoxNjAzMDMwNjI2LCJleHAiOjE2MDMwMzQyMjZ9.3mIdtfcbIRLDsqCBd9Rd8CKESpdCsQBKye_AW74mpqs");
-            // console.log(value);
+            await AsyncStorage .setItem('MyProfile', name);
+            // console.log("name", name);
             navigation.navigate("Home")
         } catch (error) {
             // Error saving data
@@ -23,18 +27,26 @@ const Intro = ({navigation}) => {
 
     return (
         <Container>
-            {/* <Header /> */}
             <LinearGradient
                 colors={["#0033ff", "#6bc1ff"]}
-                style={{ height: "25%" }}
+                style={{ height: "50%" }}
             />
+            <Text style={{
+                padding:15,
+                alignContent: "center",
+                fontSize: 15,
+                fontWeight: "bold",
+                textAlign: "center",
+                color: "#006aff"
+            }}> Welcome To Event Tracker Apps</Text>
             <Content>
-                <Form>
-                    <Item floatingLabel>
-                        <Label>Input Your Name</Label>
-                        <Input />
-                    </Item>
-                </Form>
+                <TextInput
+                    label='Name'
+                    value={name}
+                    style={{margin:10}}
+                    mode="outlined"
+                    onChangeText={text => setName(text)}
+                />
 
                 <TouchableOpacity
                     onPress={() => simpanData()}
